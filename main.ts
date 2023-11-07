016d70abe13c707fb918e9cda14fb62e8c1264ef
lea178 <146083641+lea178@users.noreply.github.com>
2023-11-07 12:44:22 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+let zufall = 0
+input.onGesture(Gesture.Shake, function () {
+    zufall = randint(0, 9)
+    if (zufall == 0) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else if (zufall == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (zufall == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
 })
